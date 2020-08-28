d61fca798a73e1e7d7e9be70bf58ab8ad53e5eba
mateusz-Precision-M2800 <mateusz.karczmarczyk@gmail.com>
2020-08-28 21:38:46 +0200

    highlight

--- src/Utils/CommandFilterTool.php
+++ src/Utils/CommandFilterTool.php
@@ -16,7 +16,7 @@ class CommandFilterTool
     {
         $string = htmlentities($string);
         $string = preg_replace('/&lt\;(.*)&gt\;/',
-            '<span class="player-content">&lt;<span class="player">${1}</span>&gt;</span>',
+            '<span class="player-content"> <i class="fa fa-comments" aria-hidden="true"></i> &lt;<span class="player">${1}</span>&gt;</span>',
             $string);
 
         return $string;
@@ -47,7 +47,40 @@ class CommandFilterTool
         $string = preg_replace('/\s\[(.*)\]:/',
             ' <span class="second-content mobile-hide">[<span class="second" title="${1}">${1}</span>]</span>:',
             $string);
+
         return $string;
     }
 
+    public static function highlightAuthMe ($string)
+    {
+        $string = preg_replace('/(\[AuthMe\] .* logged in .*)/',
+            '<span class="authme-login"><i class="fa fa-sign-in" aria-hidden="true"></i> ${1}</span>',
+            $string);
+        $string = preg_replace('/\[AuthMe\]\s(.*)\slogged in /',
+            '[AuthMe] <b>${1}</b> logged in ',
+            $string);
+
+        return $string;
+    }
+
+    public static function highlightLostConnection ($string)
+    {
+        $string = preg_replace('/: (.* lost connection: Disconnected)/',
+            ': <span class="authme-logout"><i class="fa fa-sign-out" aria-hidden="true"></i> ${1}</span>',
+            $string);
+        $string = preg_replace('/: (.* left the game)/',
+            ': <span class="authme-logout"><i class="fa fa-sign-out" aria-hidden="true"></i> ${1}</span>',
+            $string);
+
+        return $string;
+    }
+
+    public static function highlightListOfPlayers ($string)
+    {
+        $string = preg_replace('/(There are \d{1,4} of a max of \d{1,4} players online: .*)/',
+            ': <span class="list">${1}</span>',
+            $string);
+
+        return $string;
+    }
 }
