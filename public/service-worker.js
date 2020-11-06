11135167bfb3c1010f39035ee3dcf3ba3612a032
Optiplex <mateusz.karczmarczyk@gmail.com>
2020-11-06 22:36:26 +0100

    PWAgit add .!

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,70 +2,30 @@ const CACHE_NAME = 'mcmcjaciekrece_20201103';
 
 // List of files which are store in cache.
 let CACHED_FILES = [
-    '/login',
     '/css/main.css',
     '/css/main-guest.css',
     '/images/logo-full.png'
 ];
 
-const console = (({ log, error }, label) => ({
-    // Enable logs
-    // log: (...args) => log(`%c${label}`, 'color: purple', ...args),
-    // error: (...args) => error(label, ...args)
-
-    // Disable logs
-    log: () => null,
-    error: () => null
-}))(self.console, '[Service Worker]');
-
-self.addEventListener('install', (evt) => {
-    console.log('Event: install', { evt });
-    evt.waitUntil(handleInstall());
+self.addEventListener('install', function (evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(filesToCache);
+        }).catch(function (err) {
+            // Snooze errors...
+            // console.error(err);
+        })
+    );
 });
 
-async function handleInstall() {
-    const cache = await caches.open(CACHE_NAME);
-    await cache.addAll(CACHED_FILES);
-    return self.skipWaiting();
-}
-
-self.addEventListener('activate', (evt) => {
-    console.log('Event: activate', { evt });
-    evt.waitUntil(handleActivate());
+self.addEventListener('fetch', function (evt) {
+    // Snooze logs...
+    // console.log(event.request.url);
+    evt.respondWith(
+        // Firstly, send request..
+        fetch(evt.request).catch(function () {
+            // When request failed, return file from cache...
+            return caches.match(evt.request);
+        })
+    );
 });
-
-async function handleActivate() {
-    const keys = await caches.keys();
-    return await Promise.all(keys
-        .filter((key) => key !== CACHE_NAME)
-        .map((key) => caches.delete(key)));
-}
-
-function isForeignRequest(url) {
-    const regexp = new RegExp(self.origin, 'i');
-    return !regexp.test(url);
-}
-
-self.addEventListener('fetch', (evt) => {
-    console.log('Event: fetch', { evt });
-
-    if (isForeignRequest(evt.request.url)) {
-        return;
-    }
-
-    evt.respondWith(handleFetch(evt));
-});
-
-async function handleFetch(evt) {
-    const request = evt.request;
-    const cache = await caches.open(CACHE_NAME);
-    const resource = await cache.match(request);
-
-    if (resource) {
-        return resource;
-    }
-
-    const response = await fetch(request.clone());
-    await cache.put(request, response.clone());
-    return response;
-}
