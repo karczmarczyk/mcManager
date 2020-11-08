aa5f24ab39c273ec163654cb4925ccbbfe02ae1a
mateusz-Precision-M2800 <mateusz.karczmarczyk@gmail.com>
2020-11-08 12:09:07 +0100

    deploy 2020-11-08

--- bin/deployRemote.sh
+++ bin/deployRemote.sh
@@ -7,5 +7,9 @@ REMOTE_PATH=$1
 BRANCH=$2
 
 cd $REMOTE_PATH;
+
+echo "\n---pull from repo\n"
 git pull origin $BRANCH;
+
+echo "\n---clean cache\n"
 /usr/local/php/bin/php $REMOTE_PATH/bin/console cache:clear;
