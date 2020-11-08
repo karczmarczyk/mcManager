ae424476afd5881c2f715fea54fd28cc59903d0b
mateusz-Precision-M2800 <mateusz.karczmarczyk@gmail.com>
2020-11-08 12:02:26 +0100

    deploy 2020-11-08

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,14 +5,14 @@ LOCAL_PATH="/home/mateusz/www/symfony4/mcManager"
 REMOTE_PATH="/var/www/minecraft/mcManager"
 BRANCH="master"
 
-echo "send to repo to branch $BRANCH\n";
+echo "\nsend to repo to branch $BRANCH\n";
 cd $LOCAL_PATH;
 git add .
 git commit -m "deploy $NOW"
 git push origin $BRANCH
 
 #REMOTE
-echo "Go to remote\n"
+echo "\nGo to remote\n"
 ssh mateusz@192.168.10.102 sh $REMOTE_PATH/bin/deployRemote.sh $REMOTE_PATH $BRANCH;
 
-echo "DONE!\n";
+echo "\nDONE!\n";
