9f11b845d79df96adea8a1c120dccb6c63fc195f
mateusz-Precision-M2800 <mateusz.karczmarczyk@gmail.com>
2021-02-07 13:11:00 +0100

    deploy 2021-02-07

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mcmcjaciekrece_20210207';
+const CACHE_NAME = 'mcmcjaciekrece_20210207.1';
 
 // List of files which are store in cache.
 let CACHED_FILES = [
@@ -8,8 +8,8 @@ let CACHED_FILES = [
     '/images/logo-full.png'
 ];
 
-self.addEventListener('install', function (evt) {
-    evt.waitUntil(
+self.addEventListener('install', function (event) {
+    event.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
             return cache.addAll(CACHED_FILES);
         }).catch(function (err) {
@@ -19,14 +19,25 @@ self.addEventListener('install', function (evt) {
     );
 });
 
-self.addEventListener('fetch', function (evt) {
+self.addEventListener('fetch', function (event) {
     // Snooze logs...
     // console.log(event.request.url);
-    evt.respondWith(
-        // Firstly, send request..
-        fetch(evt.request).catch(function () {
-            // When request failed, return file from cache...
-            return caches.match(evt.request);
+    event.respondWith(
+        // // Firstly, send request..
+        // fetch(evt.request).catch(function () {
+        //     // When request failed, return file from cache...
+        //     return caches.match(event.request);
+        // })
+        // Try the cache
+        caches.match(event.request).then(function(response) {
+            // Fall back to network
+            return response || fetch(event.request);
+        }).catch(function() {
+            // If both fail, show a generic fallback:
+            return caches.match('/offline.html');
+            // However, in reality you'd have many different
+            // fallbacks, depending on URL & headers.
+            // Eg, a fallback silhouette image for avatars.
         })
     );
 });
